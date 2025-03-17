50706ca0c970a655b00e9f65aa82558477c27d70
TheOldZoom <xabou2010@gmail.com>
2025-03-16 21:25:18 -0600

    guildCreate & guildDelete for db

---
+++ src/events/guildDelete/index.ts
@@ -0,0 +1,27 @@
+import { Guild } from "discord.js";
+import Client from "../../struct/Client";
+import prisma from "../../struct/Prisma";
+
+export default async (guild: Guild, client: Client) => {
+  client.logs.info(`Bot has left a guild: ${guild.name} (${guild.id})`);
+
+  const existingGuild = await prisma.guild.findUnique({
+    where: { id: guild.id },
+  });
+
+  if (existingGuild) {
+    await prisma.guild.update({
+      where: { id: guild.id },
+      data: {
+        lastSeenAt: new Date(),
+      },
+    });
+    client.logs.info(
+      `Updated last seen time for guild ${guild.id} in database`
+    );
+  } else {
+    client.logs.warn(
+      `Guild ${guild.id} not found in database during deletion event`
+    );
+  }
+};
